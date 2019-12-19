2da5e15bf4e44a54c616c1a61b693c2099bcb1c7
Guruh Edi Purwanto <nagaremono@tutanota.com>
2019-12-19 14:01:53 +0800

    added function to store and display inputted number

--- calculator.js
+++ calculator.js
@@ -1,15 +1,28 @@
 "use strict"
 
 const buttons = document.querySelector('.buttons')
+const display = document.querySelector('.display')
+
+let numbers = []
+let operators = []
 
 for (let i = 0; i < 10; i++) {
   let button = document.createElement('button')
-  button.classList.add(`${i}`)
+  button.classList.add('number')
   button.textContent = `${i}`
+  button.setAttribute('id', `${i}`)
 
   buttons.appendChild(button)
 }
 
+const numberButton = document.querySelectorAll('.number')
+
+numberButton.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    numbers.push(number.id)
+    display.innerHTML = `${number.id}`
+  })
+})
 
 function add(a, b) {
   return a + b
